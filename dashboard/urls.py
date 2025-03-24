4952adb651cab7634dadbc720e832443f1b797d6
batyaboyo <batztonnie@gmail.com>
2025-03-24 12:44:14 +0300

    updates

--- dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-# dashboard/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('reports/', views.reports, name='reports'),
-]
